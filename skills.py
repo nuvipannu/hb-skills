be4432fbfaed57aecf1079f98b6b4c538f171f05
Nuvi <nuvydeep@yahoo.com>
2015-04-20 17:19:55 -0700

    Updated lists skills exercises solutions

--- skills.py
+++ skills.py
@@ -1,309 +1,263 @@
-# To work on the advanced problems, set to True
-ADVANCED = False
+#!/usr/bin/env python
+
+ADVANCED = True
+INTERMEDIATE = True
 
 
-def count_unique(string1):
-    """Count unique words in a string.
+def all_odd(number_list):
+    """Return a list of only the odd numbers in the input list.
 
-    This function should take a single string and return a dictionary
-    that has all of the distinct words as keys, and the number of times
-    that word appears in the string.
+        >>> all_odd([1, 2, 7, -5])
+        [1, 7, -5]
 
-
-    For example:
-
-        >>> print_dict(count_unique("each word appears once"))
-        {'appears': 1, 'each': 1, 'once': 1, 'word': 1}
-
-    Words that appear more than once should be counted each time:
-
-        >>> print_dict(count_unique("rose is a rose is a rose"))
-        {'a': 2, 'is': 2, 'rose': 3}
-
-    It's fine to consider punctuation part of a word (e.g., a comma
-    at the end of a word can be counted as part of that word) and
-    to consider differently-capitalized words as different:
-
-        >>> print_dict(count_unique("Porcupine see, porcupine do."))
-        {'Porcupine': 1, 'do.': 1, 'porcupine': 1, 'see,': 1}
-
-    """
-    
-    unique_count = {}
-    word_list = string1.split()
-
-    for word in word_list:
-        unique_count[word] = unique_count.get(word, 0) + 1
-
-    return unique_count
-
-
-def common_items(list1, list2):
-    """Produce the set of common items in two lists.
-
-    Given two lists, return a list of the common items shared between
-    the lists.
-
-    IMPORTANT: you may not not 'if ___ in ___' or the method 'index'.
-
-
-    For example:
-
-        >>> sorted(common_items([1, 2, 3, 4], [1, 2]))
-        [1, 2]
-
-    If an item appears more than once in both lists, return it each
-    time:
-
-        >>> sorted(common_items([1, 2, 3, 4], [1, 1, 2, 2]))
-        [1, 1, 2, 2]
-
-    (And the order of which has the multiples shouldn't matter, either):
-
-        >>> sorted(common_items([1, 1, 2, 2], [1, 2, 3, 4]))
-        [1, 1, 2, 2]
+        >>> all_odd([2, -6, 8])
+        []
 
     """
 
-    common_list = []
-    for n in list 1:
-        for i in list2:
-            if n == i:
-                common_list.append(n)
-    return common_list
+    return [n for n in number_list if n % 2 != 0]
 
 
-def unique_common_items(list1, list2):
-    """Produce the set of *unique* common items in two lists.
+def all_even(number_list):
+    """Return a list of only the even numbers in the input list.
 
-    Given two lists, return a list of the *unique* common items shared between
-    the lists.
+        >>> all_even([2, 6, -1, -2])
+        [2, 6, -2]
 
-    IMPORTANT: you may not not 'if ___ in ___' or the method 'index'.
-
-
-    Just like `common_items`, this should find [1, 2]:
-
-        >>> sorted(unique_common_items([1, 2, 3, 4], [1, 2]))
-        [1, 2]
-
-    However, now we only want unique items, so for these lists, don't show
-    more than 1 or 2 once:
-
-        >>> sorted(unique_common_items([1, 2, 3, 4], [1, 1, 2, 2]))
-        [1, 2]
-
-    """
-    
-    set1 = set(list2)
-    set2 = set(list1)
-    return list(set1 & set2)
-
-
-def sum_zero(list1):
-    """Return list of x,y number pair lists from a list where x+y==0
-
-    Given a list of numbers, add up each individual pair of numbers.
-    Return a list of each pair of numbers that adds up to 0.
-
-
-    For example:
-
-        >>> sort_pairs( sum_zero([1, 2, 3, -2, -1]) )
-        [[-2, 2], [-1, 1]]
-
-    This should always be a unique list, even if there are
-    duplicates in the input list:
-
-        >>> sort_pairs( sum_zero([1, 2, 3, -2, -1, 1, 1]) )
-        [[-2, 2], [-1, 1]]
-
-    Of course, if there are one or more zeros to pair together,
-    that's fine, too:
-
-        >>> sort_pairs( sum_zero([1, 2, 3, -2, -1, 1, 0, 1, 0]) )
-        [[-2, 2], [-1, 1], [0, 0]]
+        >>> all_even([-1, 3, 5])
+        []
 
     """
 
-    sum_zero = []
-
-    for n in range(len(list1)):
-        for i in range(n +1, len(list1)):
-            if list1[n] + list1[i] == 0:
-                zero_pair = [list1[n], list1[i]]
-                zero_pair.sort()
-                if zero_pair not in sum_zero:
-                    sum_zero.append(zero_pair)
-    return sum_zero
+    return [n for n in number_list if n % 2 == 0]
 
 
-def find_duplicates(words):
-    """Given a list of words, return the list with duplicates removed.
+def print_indeces(my_list):
+    """Print the index of each list item, followed by the item itself.
+    Do this without using a counting variable. I.e. don't do something
+    like this:
 
-    For example:
+    count = 0
+    for item in list:
+        print count
+        count = count + 1
 
-        >>> sorted(find_duplicates(
-        ...     ["rose", "is", "a", "rose", "is", "a", "rose"]))
-        ['a', 'is', 'rose']
+    Output should look like this:
 
-    You should treat differently-capitalized words as different:
+    >>> print_indeces(["Toyota", "Jeep", "Volvo"])
+    0 Toyota
+    1 Jeep
+    2 Volvo
 
-        >>> sorted(find_duplicates(
-        ...     ["Rose", "is", "a", "rose", "is", "a", "rose"]))
-        ['Rose', 'a', 'is', 'rose']
+    """
+    for i in range(len(my_list)):
+        print i, my_list[i]
+
+
+def long_words(word_list):
+    """Return all words in input list that are longer than 4 characters.
+
+        >>> long_words(["hello", "hey", "spam", "spam", "bacon", "bacon"])
+        ['hello', 'bacon', 'bacon']
+
+        >>> long_words(["all", "are", "tiny"])
+        []
 
     """
 
-    duplicates_removed = {w for w in words}
-
-    return list(duplicates_removed)
+    return [word for word in word_list if len(word) > 4]
 
 
-def word_length(words):
-    """Given list of words, return list of ascending [(len, [words])].
+def smallest_int(number_list):
+    """Find the smallest integer in a list of integers and return it.
 
-    Given a list of words, return a list of tuples, ordered by word-length.
-    Each tuple should have two items--the length of the words for that
-    word-length, and the list of words of that word length.
+        >>> smallest_int([-5, 2, -5, 7])
+        -5
 
-    For example:
+    If the input list is empty, return None:
 
-        >>> word_length(["ok", "an", "apple", "a", "day"])
-        [(1, ['a']), (2, ['ok', 'an']), (3, ['day']), (5, ['apple'])]
+        >>> smallest_int([]) is None
+        True
 
     """
 
-    word_length_dict = {}
-    word_length_list = []
-
-    for word in words:
-        word_length_dict.setdefault(len(word),[]).append(word)
-
-    for key, value in word_length_dict.items():
-        word_length_list.append((key,value))
-
-    return word_length_list
+    smallest = None
+    for item in number_list:
+        if smallest is None or item < smallest:
+            smallest = item
+    return smallest
 
 
-def pirate_talk(phrase):
-    """Translate phrase to pirate talk.
+def largest_int(number_list):
+    """Find the largest integer in a list of integers and return it.
 
-    Given a phrase, translate each word to the Pirate-speak equivalent.
-    Words that cannot be translated into Pirate-speak should pass through
-    unchanged. Return the resulting sentence.
+        >>> largest_int([-5, 2, -5, 7])
+        7
 
-    Here's a table of English to Pirate translations:
+    If the input list is empty, return None:
 
-    English     Pirate
-    ----------  ----------------
-    sir         matey
-    hotel       fleabag inn
-    student     swabbie
-    boy         matey
-    madam       proud beauty
-    professor   foul blaggart
-    restaurant  galley
-    your        yer
-    excuse      arr
-    students    swabbies
-    are         be
-    lawyer      foul blaggart
-    the         th'
-    restroom    head
-    my          me
-    hello       avast
-    is          be
-    man         matey
-
-    For example:
-
-        >>> pirate_talk("my student is not a man")
-        'me swabbie be not a matey'
-
-    You should treat words with punctuation as if they were different
-    words:
-
-        >>> pirate_talk("my student is not a man!")
-        'me swabbie be not a man!'
+        >>> largest_int([]) is None
+        True
 
     """
 
-    pirate_phrase_dict = {
-        "sir": "matey",
-        "hotel": "fleabag inn",
-        "student": "swabbie",
-        "boy": "matey",
-        "madam": "proud beauty",
-        "professor": "foul blaggart",
-        "restaurant":  "galley",
-        "your": "yer",
-        "excuse": "arr",
-        "students": "swabbies",
-        "are": "be",
-        "lawyer": "foul blaggart",
-        "the": "th'",
-        "restroom": "head",
-        "my": "me",
-        "hello":  "avast",
-        "is": "be",
-        "man": "matey"
+    largest = None
+    for item in number_list:
+        if largest is None or item > largest:
+            largest = item
+    return largest
 
-    }
 
-    phrase_list = phrase.split()
-    translation_list = []
+def halvesies(number_list):
+    """Return list of numbers from input list, each divided by two.
 
-    for word in phrase_list:
-        translation = pirate_phrase_dict.get(word,word)
-        translation_list.append(translation)
+        >>> halvesies([2, 6, -2])
+        [1.0, 3.0, -1.0]
 
-    return " ".join(translation_list)
+    If any of the numbers are, make sure you don't round off the half:
 
-def adv_word_length_sorted_words(words):
-    """Given list of words, return list of ascending [(len, [sorted-words])].
-
-    Given a list of words, return a list of tuples, ordered by word-length.
-    Each tuple should have two items--the length of the words for that
-    word-length, and the list of words of that word length. The list of words
-    for that length should be sorted alphabetically.
-
-    For example:
-
-        >>> adv_word_length_sorted_words(["ok", "an", "apple", "a", "day"])
-        [(1, ['a']), (2, ['an', 'ok']), (3, ['day']), (5, ['apple'])]
+        >>> halvesies([1, 5])
+        [0.5, 2.5]
 
     """
 
-    word_length_dict = {}
-    word_length_list = []
+    return [i / 2.0 for i in number_list]
 
-    for word in words:
-        word_length_dict.setdefault(len(word),[]).append(word)
 
-    for key, value in word_length_dict.items():
-        value.sort()
-        word_length_list.append((key, value))
+def word_lengths(word_list):
+    """Return the length of words in the input list.
 
-    return word_length_list
+        >>> word_lengths(["hello", "hey", "hello", "spam"])
+        [5, 3, 5, 4]
+
+    """
+
+    return [len(w) for w in word_list]
+
+
+def sum_numbers(number_list):
+    """Return the sum of all of the numbers in the list.
+
+    Python has a built-in function, `sum()`, which already does this -- but for
+    this exercise, you should not use it.
+
+        >>> sum_numbers([1, 2, 3, 10])
+        16
+
+    Any empty list should return the sum of zero:
+
+        >>> sum_numbers([])
+        0
+
+    """
+
+    total = 0
+    for i in number_list:
+        total = total + i
+
+    return total
+
+
+def mult_numbers(number_list):
+    """Return product (result of multiplication) of the numbers in the list.
+
+        >>> mult_numbers([1, 2, 3])
+        6
+
+    Obviously, if there is a zero in the input, the product will be zero:
+
+        >>> mult_numbers([10, 20, 0, 50])
+        0
+
+    As explained at http://en.wikipedia.org/wiki/Empty_product, if the list is
+    empty, the product should be 1:
+
+        >>> mult_numbers([])
+        1
+
+    """
+
+    product = 1
+    for i in number_list:
+        product = product * i
+    return product
+
+
+def join_strings(word_list):
+    """Return a string of all input strings joined together.
+
+    Python ha a built-in method on lists, `join` -- but this exercise, you
+    should not use it.
+
+        >>> join_strings(["spam", "spam", "bacon", "balloonicorn"])
+        'spamspambaconballoonicorn'
+
+    For an empty list, you should return an empty string:
+
+        >>> join_strings([])
+        ''
+
+    """
+
+    result = ""
+    for s in word_list:
+        result = result + s
+    return result
+
+
+def average(number_list):
+    """Return the average (mean) of the list of numbers given.
+
+        >>> average([2, 12, 3])
+        5.666666666666667
+
+    There is no defined answer if the list given is empty. It's fine if
+    this raises an error when given an empty list.
+    """
+
+    total = sum_numbers(number_list)
+    return total / float(len(number_list))
 
 
 ##############################################################################
-# You can ignore everything after here
-
-def print_dict(d):
-    # This method is just used to print dictionaries in key-alphabetical
-    # order, and is only used for our documentation tests. You can ignore it.
-    if isinstance(d, dict):
-        print "{" + ", ".join("%r: %r" % (k, d[k]) for k in sorted(d)) + "}"
-    else:
-        print d
+# END OF SKILLS TEST; YOU CAN STOP HERE.
 
 
-def sort_pairs(l):
-    # Print sorted list of pairs where the pairs are sorted. This is used only
-    # for documentation tests. You can ignore it.
-    return sorted(sorted(pair) for pair in l)
+def intermediate_join_strings(list_of_words):
+    """Return a single string with each word from the input list
+    separated by a comma.
+
+    Do this with a list comprehension. See
+    https://docs.python.org/2/tutorial/datastructures.html#list-comprehensions
+    for more info.
+
+    >>> intermediate_join_strings(["Labrador", "Poodle", "French Bulldog"])
+    'Labrador, Poodle, French Bulldog'
+
+    As above, if the list given is empty, it's fine if this function
+    raises an error.
+    """
+    output = list_of_words[0]
+    for word in list_of_words[1:]:
+        output = "%s, %s" % (output, word)
+    return output
+
+
+def adv_find_unique_long_words(my_string):
+    """Return a list of words that only appeared only once
+    within the input string and are at least 6 characters long.
+
+    >>> adv_find_unique_long_words("I ate popcorn, more popcorn, nachos, kale, and coffee.")
+    ['nachos', 'coffee']
+
+    """
+    unique_long_words = []
+    split_words = my_string.replace('.', '').replace(',', '').split(" ")
+    for word in split_words:
+        if len(word) >= 6 and split_words.count(word) == 1:
+            unique_long_words.append(word)
+    return unique_long_words
+
 
 if __name__ == "__main__":
     print
@@ -311,6 +265,8 @@ if __name__ == "__main__":
     for k, v in globals().items():
         if k[0].isalpha():
             if k.startswith('adv_') and not ADVANCED:
+                continue
+            if k.startswith('intermediate') and not INTERMEDIATE:
                 continue
             a = doctest.run_docstring_examples(v, globals(), name=k)
     print "** END OF TEST OUTPUT"
